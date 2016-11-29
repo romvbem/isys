9d347d32fce17f815fc0de38ae9e48ba870551e9
romvbem <rv.bem@sovetnikprof.ru>
2016-11-29 12:35:42 +0300

    продолжаю писать модели

--- isysproject/org_catalog/models.py
+++ isysproject/org_catalog/models.py
@@ -14,11 +14,11 @@ from django.db import models
 class Organization(models.Model):
 	full_org_name = models.Charfield(max_lenght = 200, verbose_name = "Полное наименование организации")
     short_org_name = models.Charfield(max_lenght = 50, verbose_name = "Краткое(внутреннее) наименование организации")
-    mnemo_org_name = models.Charfield(max_lenght = 200, verbose_name = "МнемоКод")
+    mnemo_org_name = models.Charfield(max_lenght = 200, verbose_name = "Мнемокод организации")
     kind_of_economic_activity = models.Charfield(max_lenght = 10, choises = СATEGORIES_kind_of_economic_activity, default = 1, verbose_name = "Вид хозяйственной деятельности")
-    legal_address = models.Charfield(max_lenght = 400, verbose_name = "Юридический адрес")
-    post_address = models.Charfield(max_lenght = 400, verbose_name = "Почтовый адрес")
-    actual_address = models.Charfield(max_lenght = 400, verbose_name = "Фактический адрес")
+    legal_address = models.OneToOneField(Address, verbose_name = "Юридический адрес")
+    post_address = models.OneToOneField(Address, verbose_name = "Почтовый адрес")
+    actual_address = models.OneToOneField(Address, verbose_name = "Фактический адрес")
     inn = models.IntegerField(max_lenght = 10, verbose_name = "ИНН")
     ogrn = models.IntegerField(max_lenght = 13, verbose_name = "ОГРН")
     kpp = models.IntegerField(max_lenght = 9, verbose_name = "КПП")
@@ -26,8 +26,8 @@ class Organization(models.Model):
     ks = models.IntegerField(max_lenght = 20, verbose_name = "Корреспондентский счет")
     rs = models.IntegerField(max_lenght = 20, verbose_name = "Расчетный счет")
     rs_where = models.Charfield(max_lenght = 150, verbose_name = "Расчетный счет где")
-    phone_number = 
-    e_mail
+    phone_number = models.ForeignKey(Phone_number, verbose_name = "Номер телефона")
+    e_mail = models.ForeignKey(Email, verbose_name = "Адрес электронной почты")
     site = models.URLField(verbose_name = "Адрес действующего интернет сайта")
     note = models.Charfield(verbose_name = "Примечание (комментарии, переговоры)")
     contact_person
@@ -53,3 +53,8 @@ class Email (models.Model):
     name = models.EmailField(verbose_name = "Email")
     email_type = models.Charfield(max_lenght = 20, choises = ((1,"Основной"),(2,"Второстепенный")), verbose_name = "Тип электронного адреса")
 
+class Person (models.Model)
+    name = models.Charfield(max_lenght = 30, verbose_name = "")
+    middle_name
+    surname
+    mnemo_person_name
